3c9bffe6a618dd6b5b548673429af7f235336ac9
greyspectrum <greyspectrum@users.noreply.github.com>
2017-01-03 20:37:56 -0500

    Improve intro documentation.

--- install-ansible.sh
+++ install-ansible.sh
@@ -9,10 +9,14 @@
 # :version: 0.1.0
 ##############################################################################
 
-# To get started, copy this file to your remote host where you want to install Ansible:
+# To get started, copy this file to your remote host where you want to install Ansible,
+# using the following command:
+#
 # scp ~/pathtoscript/install-ansible.sh root@remoteip:install-ansible.sh
+#
 # Be sure to replace "pathtoscript" with the path to this script on your local machine,
 # as well as "remoteip", which should be replaced with the IP address of the remote host.
+#
 # After you have copied the file to the remote host, just ssh to it and run this script.
 
 echo "Starting Ansible Installation..."
