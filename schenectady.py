d59736c05026e9d2ae9d16a976f220c6f0f36121
greyspectrum <greyspectrum@users.noreply.github.com>
2017-04-10 20:26:24 -0400

    Schenectady

---
+++ schenectady.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+
+string = '''\nSCHENECTADY
+
+you pitchpoled away from me,
+head first and over,
+smacked down to the rail slag
+slickened with sputtering rain,
+
+i watched you tumble to your feet,
+through a jarring whim of physics
+and stare blankly after me,
+stunned blind in shrill tympanies
+of concussive shock,
+your stop motion panic framed with garlands
+of matted blood-
+innocent to the brutalities of gravel,
+
+hours ago we had traced together
+ellipses of fearlessness,
+astride the deck of a tanker car,
+sailing with constellations
+waltzing across spitfire darknesses,
+into the brave face of dawn,
+just like the rails I had promised you,
+
+but twenty hours riding suicide,
+on a double stack through the rain,
+will make your mind numb with foolish mercies,
+and you know i can’t ride through states,
+where i’ve got outstanding warrants,
+
+i taught you to run with the train,
+and where to jump as best i could,
+and squeezed your hand to tell you it’s alright,
+fucking hell i wanted to keep you safe,
+but the union pacific,
+she is a vengeful god,
+
+when i clung helpless to the car,
+fifty tons of American cruelty
+spinning beside my head and i watched you
+get swallowed by the hillside and vanish
+behind a boxcar in the bend, 
+i’m so sorry god your face asking me
+i don’t understand and i smother you
+with gauze and tears and rain and i love you
+in the soot smeared smile of it’s ok,
+it’s ok.'''
+
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.025)
