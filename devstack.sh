5158612e52f7338121627bba450c63c3fd426dab
greyspectrum <greyspectrum@users.noreply.github.com>
2017-07-06 09:55:45 -0400

    Add devstack.sh

---
+++ devstack.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Sets up devstack
+
+sudo su -
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/suders
+sudo apt-get install git -y
+sudo su -stack
+git clone https://git.openstack.org/openstack-dev/devstack --branch stable/newton
+cd devstack
+echo -e "[[local|localrc]]\nFLOATING_RANGE=192.168.1.224/27\nFIXED_RANGE=10.11.12.0/24\nFIXED_NETWORK_SIZE=256\nFLAT_INTERFACE=ens3\nADMIN_PASSWORD=stacksecret\nDATABASE_PASSWORD=stacksecret\nRABBIT_PASSWORD=stacksecret\nSERVICE_PASSWORD=stacksecret" > local.conf
+./stack.sh
