8c06eb169ff66f33b85f59a3b79c12cbeb4b72d8
greyspectrum <greyspectrum@users.noreply.github.com>
2017-09-02 18:06:28 -0400

    Add colors, fix unit conversion issue.

--- progress.py
+++ progress.py
@@ -5,9 +5,17 @@ import sys
 from subprocess import check_output
 import re
 
-du = check_output('du -sh', shell=True)
-print "\n------------------------------TRANSFER PROGRESS------------------------------\n"
-print "THE SIZE OF THIS DIRECTORY IS: " + du
+W  = '\033[0m'  # white (normal)
+R  = '\033[31m' # red
+G  = '\033[32m' # green
+O  = '\033[33m' # orange
+B  = '\033[34m' # blue
+P  = '\033[35m' # purple
+
+du = check_output('du -s --block-size=1G', shell=True)
+duhuman = check_output('du -sh', shell=True)
+print "\n{}------------------------------TRANSFER PROGRESS------------------------------{}\n".format(G, W)
+print "THE SIZE OF THIS DIRECTORY IS: " + duhuman
 
 n1 = re.search('\d+(?:\.\d+)?', du).group(0)
 
