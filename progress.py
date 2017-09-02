42654b2c4a8309e64d0fa7e9c10461b95faeeacd
greyspectrum <greyspectrum@users.noreply.github.com>
2017-09-02 18:45:44 -0400

    Add readme info

--- progress.py
+++ progress.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+#################################################
+# PROGRESS ===>
+#
+# A lightweight script for measuring the progress
+# of large data transfers between remote hosts.
+#
+# To use, specify the size of the data transfer
+# as "n2", on line number 32.  
+#
+# Then, run this script in the target directory
+# on the destination remote host. For continuous
+# monitoring, you can employ watch to gather 
+# measurements every two seconds:
+#
+# $ watch python progress.py
+#################################################
+
 import os
 import sys
 from subprocess import check_output
@@ -12,7 +29,7 @@ print "THE SIZE OF THIS DIRECTORY IS: " + duhuman
 
 n1 = re.search('\d+(?:\.\d+)?', du).group(0)
 
-n2 = 3800
+n2 =    # Size of the data to be transfered, in gigabytes.
 
 def percentage(part, whole):
     return 100 * float(part)/float(whole)
