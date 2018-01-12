30eb75d0bf89108c5482969183b99a19c48c063e
greyspectrum <alsmith@g.harvard.edu>
2018-01-12 13:21:31 -0500

    Add a quick and ugly script to install Tensorflow on CentOS.

---
+++ centos-tensorflow.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Adapted from: https://gist.github.com/thoolihan/28679cd8156744a62f88
+
+sudo yum -y install epel-release
+sudo yum -y install gcc gcc-c++ python-pip python-devel atlas atlas-devel gcc-gfortran openssl-devel libffi-devel
+# use pip or pip3 as you prefer for python or python3
+pip install --upgrade virtualenv
+virtualenv --system-site-packages ~/venvs/tensorflow
+source ~/venvs/tensorflow/bin/activate
+sudo pip install --upgrade numpy scipy wheel cryptography
+
+read -p "Do you need support for GPUs? [y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    pip install --upgrade tensorflow-gpu
+else
+    pip install --upgrade tensorflow
+fi
