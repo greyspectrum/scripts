fcaa8fc5c5295c23001e43d394ac48e58067c9c7
greyspectrum <alsmith@g.harvard.edu>
2018-11-10 09:13:02 -0500

    add self-healing

--- nginx-check.go
+++ nginx-check.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
         cmd := exec.Command("systemctl", "status", "nginx")
+        cmd_heal := exec.Command("systemctl", "restart", "nginx")
         err := cmd.Run()
+        heal := cmd_heal.Run()
         if err != nil {
                 fmt.Printf("%v\n", err)
+                fmt.Printf("%v\n", heal)
         }
 }
