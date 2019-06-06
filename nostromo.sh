fc99f89a8c1c7e1b8df88ca17a9040eeea9906f3
greyspectrum <austin_smith@harvard.edu>
2019-06-06 15:13:28 -0400

    fix bug

--- nostromo.sh
+++ nostromo.sh
@@ -3,5 +3,5 @@
 set -e
 
 git clone https://github.com/greyspectrum/scripts.git
-cd scripts && mv scripts alsmith_scripts && python nostromo.py
+mv scripts alsmith_scripts && cd alsmith_scripts && python nostromo.py
 rm -rf alsmith_scripts
